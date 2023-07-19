e8a84ff20ec0eaac498471c1339a639d84365244
ShawanChowdhury <s.chowdhury@uqconnect.edu.au>
2023-07-19 11:00:15 +0200

    Update

--- 03.occ_model_up.R
+++ 03.occ_model_up.R
@@ -17,9 +17,10 @@ task <- as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID"))
 
 print(paste("task:", task))
 
-species_name <- species[task]
+species_name <- species[[task]]
 
 print(paste("species_name:", species_name))
+print(paste("class(species_name):", class(species_name)))
 
 ### get data #############################################
 visit_data <- read_rds("data/complete_data.rds")
