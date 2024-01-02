ac9c949f4e61fa85e43ec3043f9a286e7a5b2320
ShawanChowdhury <s.chowdhury@uqconnect.edu.au>
2024-01-02 15:16:57 +0100

    Update

--- 04.ModelSummary_pc.R
+++ 04.ModelSummary_pc.R
@@ -4,24 +4,8 @@ library(spOccupancy)
 library(MCMCvis)
 library(docopt)
 
-doc <- "usage: 04.ModelSummary.R <species> <output_dir>"
-opts <- docopt(doc)
-
-## read parameter file
-species <- read.csv(opts$species)
-
-## try to get task id
-task <- as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID"))
-
-print(paste("task:", task))
-
-species_name <- species$species[[task]]
-
-print(paste("species_name:", species_name))
-print(paste("class(species_name):", class(species_name)))
-
 ### get data #############################################
-rds <- list.files(path = "/gpfs1/work/chowdhus/03.occ_model_up_carabid/outputs", pattern = "\\.rds$", recursive = TRUE, full.names = TRUE)
+rds <- list.files(path = "outputs", pattern = "\\.rds$", recursive = TRUE, full.names = TRUE)
 sp <- rds[stringr::str_detect(rds, species_name)]
 
 out <- read_rds(sp)
@@ -53,5 +37,5 @@ psiCovs <- psiCovs %>%
          waic = df$WAIC,
          species_name = species_name)
 
-output_file <- write.csv(psiCovs, file = paste0("/work/chowdhus/ModelSummary_", 
-                                          species_name,".csv"))
+output_file <- write.csv(out, file = paste0("/work/chowdhus/ModelSummary_", 
+                                            species_name,".csv"))
