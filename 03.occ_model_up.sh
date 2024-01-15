6a8ca295adfa6bde89088da5954e30d93f1c5470
ShawanChowdhury <s.chowdhury@uqconnect.edu.au>
2024-01-15 08:56:57 +0100

    2-yr group

--- 03.occ_model_up.sh
+++ 03.occ_model_up.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --job-name=03.occ_model_carabid_1yr
+#SBATCH --job-name=03.occ_model_carabid_2yr
 #SBATCH --chdir=/home/chowdhus/occ_model_de
 #SBATCH --output=/work/%u/%x-%A-%a.out
-#SBATCH --time=14-00:00:00
+#SBATCH --time=4-00:00:00
 
 #SBATCH --mem-per-cpu=128G
 
@@ -15,4 +15,4 @@ array_or_job_id=${SLURM_ARRAY_JOB_ID:-$SLURM_JOB_ID}
 output_dir="/work/$USER/$SLURM_JOB_NAME"
 mkdir -p "$output_dir"
 
-Rscript /home/chowdhus/occ_model_de/03.occ_model_carabid_1yr.R "$species" "$output_dir"
+Rscript /home/chowdhus/occ_model_de/03.occ_model_carabid_2yr.R "$species" "$output_dir"
