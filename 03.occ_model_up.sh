f4e2367deb2206635694647432e60e4025b3c966
ShawanChowdhury <s.chowdhury@uqconnect.edu.au>
2024-01-22 14:12:56 +0100

    Update
    Lower memory

--- 03.occ_model_up.sh
+++ 03.occ_model_up.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=occ_model_fungi
 #SBATCH --chdir=/home/chowdhus/occ_model_de
 #SBATCH --output=/work/%u/%x-%A-%a.out
-#SBATCH --time=4-00:00:00
+#SBATCH --time=2-00:00:00
 
 #SBATCH --mem-per-cpu=128G
 
