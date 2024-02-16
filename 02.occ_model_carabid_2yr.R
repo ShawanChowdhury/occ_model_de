cfe9d967304d996ce2d5bc43bc5870b2d931623e
ShawanChowdhury <s.chowdhury@uqconnect.edu.au>
2024-02-16 15:24:12 +0100

    Update
    Model syntax

--- 02.occ_model_carabid_2yr.R
+++ 02.occ_model_carabid_2yr.R
@@ -113,15 +113,15 @@ all.priors <- list(beta.normal = list(mean = 0, var = 2.72),
                    sigma.sq.psi.ig = list(a = 0.1, b = 0.1))
 
 # Setting model syntax
-n.chains <- 2
-n.batch <- 150
-batch.length <- 10
+n.chains <- 3
+n.batch <- 1500
+batch.length <- 100
 (n.samples <- n.batch * batch.length) 
 #n.samples <- 50000
 n.burn <- n.samples*3/4
-n.thin <- 3
+n.thin <- 30
 ar1 <- FALSE
-n.report <- 1000
+n.report <- 10000
 
 #############################################
 # Main model
